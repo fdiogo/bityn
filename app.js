f69ef7dde7c92c261598975b4f8da551ac21480f
Ikarulus <github.ikarulus@zer.one>
2016-10-23 11:38:03 +0200

    added documentation

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.on('ready', () => {
     const acceptedExtensions = /\.mp3$/;
 
     const window = new BrowserWindow({ title: "Bityn" });
+    /** Temporarily removed the unused Electron menu bar */
     window.setMenu(null);
     window.loadURL('file://' + templates + '/window-main.html');
     window.webContents.on('did-finish-load', function() {
